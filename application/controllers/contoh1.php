e55254c2df2e75ea30a4679c0abc8de40a596aaf
ABIZARFADLY <abizarfadly1@gmail.com>
2023-10-02 21:04:43 +0700

    menambahkan file latihan MODEL, VIEW, CONTROLLER

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "ABIZAR FADLY
+            19221539
+            KOTA BAMBU PRIDE";
+
+    }
+}
